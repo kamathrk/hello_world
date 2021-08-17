edbc91d949b23b97a265bc3b870b778f0296570f
kamathrk <89082876+kamathrk@users.noreply.github.com>
2021-08-17 15:57:39 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,3 @@
 # to print hello world
-# adding this comment to trigger the vcs trigger on teamcity
+# adding this comment to trigger the vcs trigger on teamcity.
 print ('Hello World!! This is a new repository')
