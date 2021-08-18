507661a35db607bc7f7c5a1f37c4d2109447f354
kamathrk <89082876+kamathrk@users.noreply.github.com>
2021-08-18 09:52:30 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 # to print hello world
 # adding this comment to trigger the vcs trigger on teamcity.
 # creating a new branch <testbranch> to check the trigger.
+# changes in integration to trigger the new build on teamcity
 print ('Hello World!! This is a new repository')
